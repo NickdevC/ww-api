22cd58fb5c346da019b61b303a35f4a3360af33e
NickdevC <nick_coles@msn.com>
2023-05-08 13:12:59 +0000

    Add IsOwner permission and the is_owner field to ProfileSerializer

--- profiles/views.py
+++ profiles/views.py
@@ -11,7 +11,9 @@ class ProfileList(APIView):
     """Lists all profiles"""
     def get(self, request):
         profiles = Profile.objects.all()
-        serializer = ProfileSerializer(profiles, many=True)
+        serializer = ProfileSerializer(
+            profiles, many=True, context={'request': request}
+        )
         return Response(serializer.data)
 
 
@@ -33,12 +35,16 @@ class ProfileDetail(APIView):
     
     def get(self, request, pk):
         profile = self.get_object(pk)
-        serializer = ProfileSerializer(profile)
+        serializer = ProfileSerializer(
+            profile, context={'request': request}
+        )
         return Response(serializer.data)
     
     def put(self, request, pk):
         profile = self.get_object(pk)
-        serializer = ProfileSerializer(profile, data=request.data)
+        serializer = ProfileSerializer(
+            profile, data=request.data, context={'request': request}
+        )
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
--- ww_api/permissions.py
+++ ww_api/permissions.py
@@ -2,6 +2,7 @@ from rest_framework import permissions
 
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
+    """Checks to see if user is owner of the profile"""
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
