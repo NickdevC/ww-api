0f2e7e41525cdd4da772f2d9c61d129457836ac5
NickdevC <nick_coles@msn.com>
2023-05-08 15:50:40 +0000

    Add Adventure model to admin.py

--- adventures/views.py
+++ adventures/views.py
@@ -6,9 +6,24 @@ from .serializers import AdventureSerializer
 
 
 class AdventureList(APIView):
+    serializer_class = AdventureSerializer
+    
     def get(self, request):
         adventures = Adventure.objects.all()
         serializer = AdventureSerializer(
             adventures, many=True, context={'request': request}
         )
         return Response(serializer.data)
+    
+    def adventure(self, request):
+        serializer = AdventureSerializer(
+            data=request.data, context={'request': request}
+        )
+        if serializer.is_valid():
+            serializer.save(owner=request.user)
+            return Response(
+                serializer.data, status=status.HTTP_201_CREATED
+            )
+        return Response(
+            serializer.errors, status=status.HTTP_400_BAD_REQUEST
+        )
