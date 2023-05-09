055657f4f81d54d2fbc9fd530a745303f2531976
NickdevC <nick_coles@msn.com>
2023-05-09 11:37:28 +0000

    Add AdventureDetail view with get_object and get functions

--- adventures/urls.py
+++ adventures/urls.py
@@ -3,4 +3,5 @@ from adventures import views
 
 urlpatterns = [
     path('adventures/', views.AdventureList.as_view()),
+    path('adventures/<int:pk>/', views.AdventureDetail.as_view()),
 ]
--- adventures/views.py
+++ adventures/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from rest_framework import status, permissions
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -7,6 +8,7 @@ from ww_api.permissions import IsOwnerOrReadOnly
 
 
 class AdventureList(APIView):
+    """List all adventure posts"""
     serializer_class = AdventureSerializer
     permission_classes = [
         permissions.IsAuthenticatedOrReadOnly
@@ -31,3 +33,27 @@ class AdventureList(APIView):
         return Response(
             serializer.errors, status=status.HTTP_400_BAD_REQUEST
         )
+
+
+class AdventureDetail(APIView):
+    """
+    Retrieves specific adventure post details, allowing users
+    to edit and delete the post's information.
+    """
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = AdventureSerializer
+
+    def get_object(self, pk):
+        try:
+            adventure = Adventure.objects.get(pk=pk)
+            self.check_object_permissions(self.request, adventure)
+            return adventure
+        except Adventure.DoesNotExist:
+            raise Http404
+    
+    def get(self, request, pk):
+        adventure = self.get_object(pk)
+        serializer = AdventureSerializer(
+            adventure, context={'request': request}
+        )
+        return Response(serializer.data)
