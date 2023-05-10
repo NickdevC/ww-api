351f84d411cc3ac123ca83dd106922551cd95509
NickdevC <nick_coles@msn.com>
2023-05-10 09:24:44 +0000

    Add AdventureList test to see if users can list adventure posts

--- adventures/tests.py
+++ adventures/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Adventure
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class AdventureList(APITestCase):
+    def setUp(self):
+        User.objects.create_user(username='nick', password='pass')
+    
+    def test_can_list_posts(self):
+        nick = User.objects.get(username='nick')
+        Adventure.objects.create(owner=nick, title='a test title')
+        response = self.client.get('/adventures/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        print(response.data)
+        print(len(response.data))
