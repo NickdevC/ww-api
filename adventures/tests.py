6f54f09a84d4df2f93ba8bc22db724017ed4d2d1
NickdevC <nick_coles@msn.com>
2023-05-12 07:32:11 +0000

    Add AdventureDetailViewTests to check retrieval of posts with valid id

--- adventures/tests.py
+++ adventures/tests.py
@@ -19,3 +19,24 @@ class AdventureListViewsTests(APITestCase):
     def test_user_not_logged_in_cant_create_post(self):
         response = self.client.post('/adventures/', {'title': 'a test title'})
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+
+class AdventureDetailViewTests(APITestCase):
+    def setUp(self):
+        nick = User.objects.create_user(username='nick', password='pass')
+        chloe = User.objects.create_user(username='chloe', password='pass')
+        Adventure.objects.create(
+            owner=nick, title='a test title', description='nicks content'
+        )
+        Adventure.objects.create(
+            owner=chloe, title='another test title', description='chloes content'
+        )
+    
+    def test_can_retrieve_post_using_valid_id(self):
+        response = self.client.get('/adventures/1/')
+        self.assertEqual(response.data['title'], 'a test title')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_cannot_retrieve_post_using_invalid_valid_id(self):
+        response = self.client.get('/adventures/50/')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
