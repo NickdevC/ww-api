6f2554ce6cdb0fa5aa0ffa79347a913e0eedf758
NickdevC <nick_coles@msn.com>
2023-05-16 12:28:38 +0000

    Refactor 'profiles' app views, making them generic views

--- adventures/views.py
+++ adventures/views.py
@@ -1,10 +1,3 @@
-# from django.http import Http404
-# from rest_framework import status, permissions
-# from rest_framework.response import Response
-# from rest_framework.views import APIView
-# from .models import Adventure
-# from .serializers import AdventureSerializer
-# from ww_api.permissions import IsOwnerOrReadOnly
 from rest_framework import generics, permissions
 from ww_api.permissions import IsOwnerOrReadOnly
 from .models import Adventure
@@ -25,33 +18,6 @@ class AdventureList(generics.ListCreateAPIView):
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
-# class AdventureList(APIView):
-#     """List all adventure posts"""
-#     serializer_class = AdventureSerializer
-#     permission_classes = [
-#         permissions.IsAuthenticatedOrReadOnly
-#     ]
-
-#     def get(self, request):
-#         adventures = Adventure.objects.all()
-#         serializer = AdventureSerializer(
-#             adventures, many=True, context={'request': request}
-#         )
-#         return Response(serializer.data)
-    
-#     def post(self, request):
-#         serializer = AdventureSerializer(
-#             data=request.data, context={'request': request}
-#         )
-#         if serializer.is_valid():
-#             serializer.save(owner=request.user)
-#             return Response(
-#                 serializer.data, status=status.HTTP_201_CREATED
-#             )
-#         return Response(
-#             serializer.errors, status=status.HTTP_400_BAD_REQUEST
-#         )
-
 
 class AdventureDetail(generics.RetrieveUpdateDestroyAPIView):
     """
@@ -61,45 +27,3 @@ class AdventureDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = AdventureSerializer
     permission_classes = [IsOwnerOrReadOnly]
     queryset = Adventure.objects.all()
-
-# class AdventureDetail(APIView):
-#     """
-#     Retrieves specific adventure post details, allowing users
-#     to edit and delete the post's information.
-#     """
-#     permission_classes = [IsOwnerOrReadOnly]
-#     serializer_class = AdventureSerializer
-
-#     def get_object(self, pk):
-#         try:
-#             adventure = Adventure.objects.get(pk=pk)
-#             self.check_object_permissions(self.request, adventure)
-#             return adventure
-#         except Adventure.DoesNotExist:
-#             raise Http404
-    
-#     def get(self, request, pk):
-#         adventure = self.get_object(pk)
-#         serializer = AdventureSerializer(
-#             adventure, context={'request': request}
-#         )
-#         return Response(serializer.data)
-
-#     def put(self, request, pk):
-#         adventure = self.get_object(pk)
-#         serializer = AdventureSerializer(
-#             adventure, data=request.data, context={'request': request}
-#         )
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data)
-#         return Response(
-#             serializer.errors, status=status.HTTP_400_BAD_REQUEST
-#         )
-    
-#     def delete(self, request, pk):
-#         adventure = self.get_object(pk)
-#         adventure.delete()
-#         return Response(
-#             status=status.HTTP_204_NO_CONTENT
-#         )
--- profiles/views.py
+++ profiles/views.py
@@ -1,51 +1,22 @@
-from django.http import Http404
-from rest_framework import status
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from rest_framework import generics
+from ww_api.permissions import IsOwnerOrReadOnly
 from .models import Profile
 from .serializers import ProfileSerializer
-from ww_api.permissions import IsOwnerOrReadOnly
 
 
-class ProfileList(APIView):
-    """Lists all profiles"""
-    def get(self, request):
-        profiles = Profile.objects.all()
-        serializer = ProfileSerializer(
-            profiles, many=True, context={'request': request}
-        )
-        return Response(serializer.data)
-
-
-class ProfileDetail(APIView):
+class ProfileList(generics.ListAPIView):
     """
-    Retrives specific profile details and allows user
-    to edit profile information.
+    List all profiles.
+    No create view as profile creation is handled by Django signals.
     """
     serializer_class = ProfileSerializer
+    queryset = Profile.objects.all()
+
+
+class ProfileDetail(generics.RetrieveUpdateAPIView):
+    """
+    Retrieve or update a profile if user is the owner.
+    """
     permission_classes = [IsOwnerOrReadOnly]
-    
-    def get_object(self, pk):
-        try:
-            profile = Profile.objects.get(pk=pk)
-            self.check_object_permissions(self.request, profile)
-            return profile
-        except Profile.DoesNotExist:
-            raise Http404
-    
-    def get(self, request, pk):
-        profile = self.get_object(pk)
-        serializer = ProfileSerializer(
-            profile, context={'request': request}
-        )
-        return Response(serializer.data)
-    
-    def put(self, request, pk):
-        profile = self.get_object(pk)
-        serializer = ProfileSerializer(
-            profile, data=request.data, context={'request': request}
-        )
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    serializer_class = ProfileSerializer
+    queryset = Profile.objects.all()
