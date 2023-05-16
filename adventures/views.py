535f600027a07417b88f7b5a47cbc3431ce20807
NickdevC <nick_coles@msn.com>
2023-05-16 12:21:30 +0000

    Refactor views in 'adventures' app, making them generic views

--- adventures/views.py
+++ adventures/views.py
@@ -1,78 +1,105 @@
-from django.http import Http404
-from rest_framework import status, permissions
-from rest_framework.response import Response
-from rest_framework.views import APIView
+# from django.http import Http404
+# from rest_framework import status, permissions
+# from rest_framework.response import Response
+# from rest_framework.views import APIView
+# from .models import Adventure
+# from .serializers import AdventureSerializer
+# from ww_api.permissions import IsOwnerOrReadOnly
+from rest_framework import generics, permissions
+from ww_api.permissions import IsOwnerOrReadOnly
 from .models import Adventure
 from .serializers import AdventureSerializer
-from ww_api.permissions import IsOwnerOrReadOnly
 
 
-class AdventureList(APIView):
-    """List all adventure posts"""
+class AdventureList(generics.ListCreateAPIView):
+    """
+    List adventure posts or create adventure posts if
+    logged in.
+    Perform_create: associates the post with the logged in
+    user.
+    """
     serializer_class = AdventureSerializer
-    permission_classes = [
-        permissions.IsAuthenticatedOrReadOnly
-    ]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Adventure.objects.all()
 
-    def get(self, request):
-        adventures = Adventure.objects.all()
-        serializer = AdventureSerializer(
-            adventures, many=True, context={'request': request}
-        )
-        return Response(serializer.data)
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+# class AdventureList(APIView):
+#     """List all adventure posts"""
+#     serializer_class = AdventureSerializer
+#     permission_classes = [
+#         permissions.IsAuthenticatedOrReadOnly
+#     ]
+
+#     def get(self, request):
+#         adventures = Adventure.objects.all()
+#         serializer = AdventureSerializer(
+#             adventures, many=True, context={'request': request}
+#         )
+#         return Response(serializer.data)
     
-    def post(self, request):
-        serializer = AdventureSerializer(
-            data=request.data, context={'request': request}
-        )
-        if serializer.is_valid():
-            serializer.save(owner=request.user)
-            return Response(
-                serializer.data, status=status.HTTP_201_CREATED
-            )
-        return Response(
-            serializer.errors, status=status.HTTP_400_BAD_REQUEST
-        )
+#     def post(self, request):
+#         serializer = AdventureSerializer(
+#             data=request.data, context={'request': request}
+#         )
+#         if serializer.is_valid():
+#             serializer.save(owner=request.user)
+#             return Response(
+#                 serializer.data, status=status.HTTP_201_CREATED
+#             )
+#         return Response(
+#             serializer.errors, status=status.HTTP_400_BAD_REQUEST
+#         )
 
 
-class AdventureDetail(APIView):
+class AdventureDetail(generics.RetrieveUpdateDestroyAPIView):
     """
-    Retrieves specific adventure post details, allowing users
-    to edit and delete the post's information.
+    Retrieves specific adventure posts, allowing users to edit
+    and delete data of the posts they own.
     """
+    serializer_class = AdventureSerializer
     permission_classes = [IsOwnerOrReadOnly]
-    serializer_class = AdventureSerializer
+    queryset = Adventure.objects.all()
 
-    def get_object(self, pk):
-        try:
-            adventure = Adventure.objects.get(pk=pk)
-            self.check_object_permissions(self.request, adventure)
-            return adventure
-        except Adventure.DoesNotExist:
-            raise Http404
-    
-    def get(self, request, pk):
-        adventure = self.get_object(pk)
-        serializer = AdventureSerializer(
-            adventure, context={'request': request}
-        )
-        return Response(serializer.data)
+# class AdventureDetail(APIView):
+#     """
+#     Retrieves specific adventure post details, allowing users
+#     to edit and delete the post's information.
+#     """
+#     permission_classes = [IsOwnerOrReadOnly]
+#     serializer_class = AdventureSerializer
 
-    def put(self, request, pk):
-        adventure = self.get_object(pk)
-        serializer = AdventureSerializer(
-            adventure, data=request.data, context={'request': request}
-        )
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(
-            serializer.errors, status=status.HTTP_400_BAD_REQUEST
-        )
+#     def get_object(self, pk):
+#         try:
+#             adventure = Adventure.objects.get(pk=pk)
+#             self.check_object_permissions(self.request, adventure)
+#             return adventure
+#         except Adventure.DoesNotExist:
+#             raise Http404
     
-    def delete(self, request, pk):
-        adventure = self.get_object(pk)
-        adventure.delete()
-        return Response(
-            status=status.HTTP_204_NO_CONTENT
-        )
+#     def get(self, request, pk):
+#         adventure = self.get_object(pk)
+#         serializer = AdventureSerializer(
+#             adventure, context={'request': request}
+#         )
+#         return Response(serializer.data)
+
+#     def put(self, request, pk):
+#         adventure = self.get_object(pk)
+#         serializer = AdventureSerializer(
+#             adventure, data=request.data, context={'request': request}
+#         )
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(
+#             serializer.errors, status=status.HTTP_400_BAD_REQUEST
+#         )
+    
+#     def delete(self, request, pk):
+#         adventure = self.get_object(pk)
+#         adventure.delete()
+#         return Response(
+#             status=status.HTTP_204_NO_CONTENT
+#         )
--- comments/views.py
+++ comments/views.py
@@ -5,6 +5,11 @@ from .serializers import CommentSerializer, CommentDetailSerializer
 
 
 class CommentList(generics.ListCreateAPIView):
+    """
+    List all comments and allows users to create a comment
+    attached to a specific adventure post id.
+    Perform_create: associate the current logged in user with a post.
+    """
     serializer_class = CommentSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Comment.objects.all()
@@ -14,6 +19,12 @@ class CommentList(generics.ListCreateAPIView):
 
 
 class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Retrieve a comment.
+    Includes update view to allow owner of a comment to update the
+    contents.
+    Destroy allows user to delete their own comments.
+    """
     permission_classes = [IsOwnerOrReadOnly]
     serializer_class = CommentDetailSerializer
     queryset = Comment.objects.all()
